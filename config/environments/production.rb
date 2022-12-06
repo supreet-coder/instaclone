afe0005c1df2017dd5c45d20815f9fcf0335c7be
supreet-coder <supreet.bliss@gmail.com>
2022-12-06 20:12:41 +0530

    second commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Disable serving static files from the `/public` folder by default since
+# Apache or NGINX already handles this.
+config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
