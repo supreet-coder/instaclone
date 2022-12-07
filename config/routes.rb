ea5c9258bbbbc0ad1c10b1d540f242b8bd47a7ef
supreet-coder <supreet.bliss@gmail.com>
2022-12-07 14:36:17 +0530

    404 fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
 
+
+  if Rails.env.development?
+    scope format: true, constraints: { format: /jpg|png|gif|PNG/ } do
+      get '/*anything', to: proc { [404, {}, ['']] }, constraints: lambda { |request| !request.path_parameters[:anything].start_with?('rails/') }
+    end
+  end
+
   devise_scope :user do
      get '/users/sign_out' => 'devise/sessions#destroy'
   end
